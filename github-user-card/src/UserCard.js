15b88aaf40e66110614b2c9ea8032e83ef193da4
abe-one <77995299+abe-one@users.noreply.github.com>
2021-04-06 15:50:10 -0500

    Render UserCard data

--- github-user-card/src/UserCard.js
+++ github-user-card/src/UserCard.js
@@ -1,14 +1,29 @@
 import React from "react";
 
-const UserCard = ({ userData }) => {
+const UserCard = ({
+  userData: {
+    avatar_url,
+    login,
+    name,
+    bio,
+    location,
+    html_url,
+    followers,
+    following,
+  },
+}) => {
   // console.log(userData);
   return (
     <div className="user-card">
-      <img
-        width="350px"
-        src={userData.avatar_url}
-        alt={`${userData.login}'s avatar`}
-      ></img>
+      <img width="200px" src={avatar_url} alt={`${login}'s avatar`} />
+      <div>
+        <h3>{name}</h3>
+        <p>{login}</p>
+        <p>Location: {location}</p>
+        <p>Follows:{followers}</p>
+        <p>Following: {following}</p>
+        <p>{bio}</p>
+      </div>
     </div>
   );
 };
